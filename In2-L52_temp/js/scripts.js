b6d0f958691450c9fc15cf9ca2e7a020c580f667
Samuel Majerus <ladolego@gmail.com>
2022-08-10 10:17:14 -0700

    Added 'event.preventDefault()' for 'onsubmit' object.     Updated JS file -- user inputs for each form value will now get retrieved. (Each Field will now display the respective user inputs, rather than the respective Fields' variable names.)

--- In2-L52_temp/js/scripts.js
+++ In2-L52_temp/js/scripts.js
@@ -1,20 +1,27 @@
 window.onload = function() {
-  // first we set up an event handler for the form submission
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    // then we print values to the story area;
-    // we're hardcoding these values for now
-    document.querySelector("span#person1a").innerText = "person 1";
-    document.querySelector("span#person1b").innerText = "person 1";
-    document.querySelector("span#person1c").innerText = "person 1";
-    document.querySelector("span#person2a").innerText = "person 2";
-    document.querySelector("span#person2b").innerText = "person 2";
-    document.querySelector("span#animal").innerText = "an animal";
-    document.querySelector("span#verb").innerText = "verb";
-    document.querySelector("span#noun").innerText = "noun";
-    document.querySelector("span#exclamation").innerText = "exclamation";
+    // in this section we get the value for each form input
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
 
-    // then we show the story by removing the class attribute
+    // then we set the story variables to the values we got from the form
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
     document.querySelector("div#story").removeAttribute("class");
+
+    event.preventDefault();
   };
 };
