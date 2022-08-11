e1aa30a55c9663dc7612e6037fb9b27b1ace218b
Samuel Majerus <ladolego@gmail.com>
2022-08-11 14:01:48 -0700

    Initial commit.  This is the version given by lesson 69, with an error intentionally added in the JS file for demonstration purposes (namely, setting up DevTools to pause on exceptions when debugging JS, what to do to find the error without going back into VS Code, etc).

---
+++ calc-lsn69/js/scripts.js
@@ -0,0 +1,42 @@
+// Business Logic
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = ad(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+  document.getElementById("output").innerText = result;
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
