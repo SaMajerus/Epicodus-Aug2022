cc0a92eef2144bfda43c97934a9191cbe8b62b71
Samuel Majerus <ladolego@gmail.com>
2022-08-11 10:35:42 -0700

    Added functionality for 'handleCalculation()' function to retrieve entered values from the form.

--- calc-clone/js/scripts.js
+++ calc-clone/js/scripts.js
@@ -16,10 +16,12 @@ function divide(num1, num2) {
 }
 
 // User Interface Logic
-function handleCalculation(event) {
-  event.preventDefault();
-  // the code to get and process form values will go here!
-}
+function handleCalculation(event) { 
+  event.preventDefault(); 
+  const number1 = parseInt(document.querySelector("input#input1").value); 
+  const number2 = parseInt(document.querySelector("input#input2").value); 
+  const operator = document.querySelector("input[name='operator']:checked").value; 
+} 
 
 window.addEventListener("load", function() {
   const form = document.getElementById("calculator");
