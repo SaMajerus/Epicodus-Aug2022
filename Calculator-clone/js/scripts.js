76035092029cac7251d8a167efa2a21c38afeaa2
Samuel Majerus <ladolego@gmail.com>
2022-08-11 10:08:16 -0700

    Initial commit. Cloned my fork of William M's 'Calculator' repo from a few days ago (with me and Athea co-authoring), copied the files into the directory being committed, then deleted the original cloned 'Calculator' directory.    (Doing all this so that I at least have a starting file for 'Intro Sec 2, Lsn 66'.)

---
+++ Calculator-clone/js/scripts.js
@@ -0,0 +1,52 @@
+// business logic
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+// user interface logic 
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add("The sum of the two numbers is " + number1, number2+));
+
+
+
+
+//business logic
+function subtract(number3, number4){
+  return number3 - number4;
+}
+
+//use interface logic
+const number3 = prompt("Enter a number:");
+const number4 = prompt("Enter another number:");
+
+window.alert(subtract(number3, number4)); 
+
+
+
+
+//business logic
+function multiply(number5, number6) {
+  return number5 * number6;
+}
+
+//user interface logic
+const number5 = prompt("Enter a number:");
+const number6 = prompt("Enter another number:");
+
+window.alert(multiply(number5, number6)); 
+
+
+
+
+//business logic
+function divide(number7, number8) {
+  return number7 / number8;
+}
+
+//user interface logic
+const number7 = prompt("Enter a number:");
+const number8 = prompt("Enter another number:");
+
+window.alert(divide(number7, number8)); 
